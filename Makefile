e65f219089328800a86d228d4ce60d82ad04c52e
Hubert Vecchioli <hvecchio@made-f0Ar11s4.clusters.42paris.fr>
2024-07-19 18:05:03 +0200

    Update c files org & Makefile de a mort

--- Makefile
+++ Makefile
@@ -1,80 +1,111 @@
-#so_long
+###############################################################################
+###############################################################################
+#*****************************************************************************#
+#********************** Makefile for minishell project  **********************#
+#*****************************************************************************#
+###############################################################################
+###############################################################################
+
 NAME 		= minishell
-SRCS 			=	$(addprefix srcs/main/, $(addsuffix .c, \
-					main \
-					minishell \
-					init \
-					env_var \
-					generate_prompt \
-					ft_clean_prompt \
-					easy_errors \
-					easy_errors_utils \
-					expand_tilde \
-					init_ast))
-SRCS 			+=	$(addprefix srcs/parse/, $(addsuffix .c, \
-					print_ast \
-					utils_ast))
-SRCS 			+=	$(addprefix srcs/utils/, $(addsuffix .c, \
-					utils_list))
-SRCS 			+=	$(addprefix srcs/execute/, $(addsuffix .c, \
-					exec_ast \
-					exec_builtin \
-					ft_heredoc \
-					ft_pipe \
-					solve_path \
-					exec_redir))
-SRCS 			+=	$(addprefix srcs/end/, $(addsuffix .c, \
-					end_status \
-					ft_exit))
-SRCS 			+=	$(addprefix srcs/clean/, $(addsuffix .c, \
-					clean_ast \
-					clean_list \
-					save_ast_lk \
-					ret_status))
-SRCS 			+=	$(addprefix srcs/built-in/cd/, $(addsuffix .c, \
-					ft_cd_step1 \
-					ft_cd_step2 \
-					ft_cd \
-					ft_cd_utils1))
-SRCS 			+=	$(addprefix srcs/built-in/echo/, $(addsuffix .c, \
-					ft_echo ))
-SRCS 			+=	$(addprefix srcs/built-in/env/, $(addsuffix .c, \
-					ft_env ))
-SRCS 			+=	$(addprefix srcs/built-in/export/, $(addsuffix .c, \
-					ft_export ))
-SRCS 			+=	$(addprefix srcs/built-in/pwd/, $(addsuffix .c, \
-					ft_pwd ))
-SRCS 			+=	$(addprefix srcs/built-in/unset/, $(addsuffix .c, \
-					ft_unset ))
-SRCS 			+=	$(addprefix srcs/built-in/, $(addsuffix .c, \
-					utils_builtin ))
-OBJS			=  ${SRCS:.c=.o}
-
-#compiler
 CC			= cc
-FLAGS       = -Wall -Wextra -Werror -Imlx -I ./includes -I $(LIBFT_DIR) 
+CFLAGS		= -Wall -Wextra -Werror
+###############################################################################
+###############################################################################
 
-# 
 LIBFT_DIR	= ./libft
-LIBFT_PATH	= $(LIBFT_DIR)/libft.a
+LIBFT		= $(LIBFT_DIR)/libft.a
+###############################################################################
+###############################################################################
+
+MAINFILES 		= main.c \
+				minishell.c \
+				init.c \
+				env_var.c \
+				generate_prompt.c \
+				ft_clean_prompt.c \
+				easy_errors.c \
+				easy_errors_utils.c \
+				init_ast.c  \
+				expand_tilde.c \
+				expand_env.c
+PARSEFILES		= print_ast.c \
+				utils_ast.c
+UTILSFILES 		= utils_list.c
+EXECUTEFILES 	= exec_ast.c \
+				exec_builtin.c \
+				ft_heredoc.c \
+				ft_pipe.c \
+				solve_path.c \
+				exec_redir.c
+ENDFILES 		= end_status.c \
+				ft_exit.c
+CLEANFILES 		= clean_ast.c \
+				clean_list.c \
+				save_ast_lk.c \
+				ret_status.c
+BUILTINFILES	= ft_cd_step1.c \
+				ft_cd_step2.c \
+				ft_cd.c \
+				ft_cd_utils1.c \
+				ft_echo.c \
+				ft_env.c \
+				ft_export.c \
+				ft_pwd.c \
+				ft_unset.c \
+				utils_builtin.c
+###############################################################################
+###############################################################################
+
+INCS = ./includes
+OBJDIRS = objs
+SRCDIRS = srcs
+PATHS	= $(addprefix main/, $(MAINFILES))\
+		$(addprefix parse/, $(PARSEFILES))\
+		$(addprefix utils/, $(UTILSFILES))\
+		$(addprefix execute/, $(EXECUTEFILES))\
+		$(addprefix end/, $(ENDFILES))\
+		$(addprefix clean/, $(CLEANFILES))\
+		$(addprefix built-in/, $(BUILTINFILES))
+OBJS = $(addprefix $(OBJDIRS)/, $(patsubst %.c, %.o, $(PATHS)))
+SRCS = $(addprefix $(SRCDIRS)/, $(PATHS))
+###############################################################################
+###############################################################################
+
+$(NAME) : $(OBJDIRS) $(OBJS) $(LIBFT)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) -I$(INCS) -L$(LIBFT_DIR) -lft -lreadline
+
+$(OBJDIRS):
+	mkdir -p $(OBJDIRS)
+	mkdir -p $(addprefix $(OBJDIRS)/, main)
+	mkdir -p $(addprefix $(OBJDIRS)/, parse)
+	mkdir -p $(addprefix $(OBJDIRS)/, utils)
+	mkdir -p $(addprefix $(OBJDIRS)/, execute)
+	mkdir -p $(addprefix $(OBJDIRS)/, end)
+	mkdir -p $(addprefix $(OBJDIRS)/, clean)
+	mkdir -p $(addprefix $(OBJDIRS)/, built-in)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+###############################################################################
+###############################################################################
 
 all : $(NAME)
 
-.c.o:
-	$(CC) $(FLAGS) -c $< -o ${<:.c=.o}
-##  -lreadline to compile readline
-$(NAME): $(OBJS)
-	@make -C $(LIBFT_DIR)
-	$(CC) $(FLAGS) $(OBJS) $(LIBFT_PATH) -o $(NAME) -lreadline
-	@make clean
-
 clean :
-	@make clean -C $(LIBFT_DIR)
-	rm -f ${OBJS}
+	$(MAKE) -C $(LIBFT_DIR) clean
+	rm -rf $(OBJDIRS)
 
 fclean : clean
-	rm -f ${NAME}
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	rm -f $(NAME)
 
 re : fclean all
 
+$(OBJDIRS)/%.o: $(SRCDIRS)/%.c
+	$(CC) $(CFLAGS) -I$(INCS) -c $< -o $@
+###############################################################################
+###############################################################################
+
 .PHONY: all clean fclean re
+###############################################################################
+###############################################################################
--- srcs/main/expand_env.c
+++ srcs/main/expand_env.c
@@ -6,125 +6,125 @@
 /*   By: hvecchio <hvecchio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 12:10:36 by hvecchio          #+#    #+#             */
-/*   Updated: 2024/07/19 16:54:44 by hvecchio         ###   ########.fr       */
+/*   Updated: 2024/07/19 18:01:01 by hvecchio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	ft_is_in_squote(char *line, int i)
-{
-	int		in_quotes;
-	char	q_type;
-	int		j;
+// static int	ft_is_in_squote(char *line, int i)
+// {
+// 	int		in_quotes;
+// 	char	q_type;
+// 	int		j;
 
-	in_quotes = 0;
-	q_type = 0;
+// 	in_quotes = 0;
+// 	q_type = 0;
 	
-	while (line[++j] && j < i)
-	{
-		if ((line[j] == 34 || line[j] == 39))
-		{
-			if (in_quotes && q_type == line[j])
-				in_quotes = 0;
-			else if (!in_quotes)
-			{
-				q_type = line[j];
-				in_quotes = 1;
-			}
-		}
-	}
-	if (in_quotes && q_type == 39)
-		return (1);
-	return (0);
-}
+// 	while (line[++j] && j < i)
+// 	{
+// 		if ((line[j] == 34 || line[j] == 39))
+// 		{
+// 			if (in_quotes && q_type == line[j])
+// 				in_quotes = 0;
+// 			else if (!in_quotes)
+// 			{
+// 				q_type = line[j];
+// 				in_quotes = 1;
+// 			}
+// 		}
+// 	}
+// 	if (in_quotes && q_type == 39)
+// 		return (1);
+// 	return (0);
+// }
 
-int	ft_expand_var(char *line, t_list *lst_env)
-{
-	int		i;
+// int	ft_expand_var(char *line, t_list *lst_env)
+// {
+// 	int		i;
 
-	i = -1;
-	if (!line)
-		return (0);
-	while (line[++i])
-	{
-		if (line[i] != '$' || !line[i + 1])
-			continue ;
-		if (ft_is_in_squote(line, i))
-			continue ;
-		i++;
-		ft_expand(line, i);
-	}
-	return (1);
-}
+// 	i = -1;
+// 	if (!line)
+// 		return (0);
+// 	while (line[++i])
+// 	{
+// 		if (line[i] != '$' || !line[i + 1])
+// 			continue ;
+// 		if (ft_is_in_squote(line, i))
+// 			continue ;
+// 		i++;
+// 		// ft_expand(line, i);
+// 	}
+// 	return (1);
+// }
 
-int	ft_expand(char *line, int *i, t_list *lst_env)
-{
-	char 	*before_var;
-	char 	*var_name;
-	char 	*after_var;
-	int		j;
-	int		size;
+// int	ft_expand(char *line, int *i, t_list *lst_env)
+// {
+// 	char 	*before_var;
+// 	char 	*var_name;
+// 	char 	*after_var;
+// 	int		j;
+// 	int		size;
 
-	size = ft_strlen(line);
-	if (line[*i] && ft_isdigit(line[*i]))
-		return (ft_remove_var(md, line, i, 1));
-	j = 0;
-	while (line[*i + j] && ft_isalnum(line[*i + j]))
-		j++;
-	if (!j)
-		return (0);
-	var_name = ft_substr(line, *i, j);
-	if (!var_name)
-		return (ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
-	if (!ft_lst_find_env_var(md->env_head, var_name))
-		return (free(var_name), ft_remove_var(md, line, *i, j));
-	return (free(var_name), ft_add_var(md, line, *i, j, ft_lst_find_env_var(md->env_head, var_name)));
+// 	size = ft_strlen(line);
+// 	if (line[*i] && ft_isdigit(line[*i]))
+// 		return (ft_remove_var(md, line, i, 1));
+// 	j = 0;
+// 	while (line[*i + j] && ft_isalnum(line[*i + j]))
+// 		j++;
+// 	if (!j)
+// 		return (0);
+// 	var_name = ft_substr(line, *i, j);
+// 	if (!var_name)
+// 		return (ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
+// 	if (!ft_lst_find_env_var(md->env_head, var_name))
+// 		return (free(var_name), ft_remove_var(md, line, *i, j));
+// 	return (free(var_name), ft_add_var(md, line, *i, j, ft_lst_find_env_var(md->env_head, var_name)));
 
-}
+// }
 
-int	ft_remove_var(t_master_shell *md, t_cmd_tbl *tab, int *i, int j)
-{
-	char 	*bf_var;
-	char 	*aft_var;
-	int		size;
+// int	ft_remove_var(t_master_shell *md, t_cmd_tbl *tab, int *i, int j)
+// {
+// 	char 	*bf_var;
+// 	char 	*aft_var;
+// 	int		size;
 
-	size = ft_strlen(tab->cmd);
-	bf_var = ft_substr(tab->cmd, 0, *i - 1);
-	aft_var = ft_substr(tab->cmd, *i + j, size - (*i + j));
-	if (!bf_var || !aft_var)
-		return (free(bf_var), free(aft_var), ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
-	free(tab->cmd);*i = ft_strlen(bf_var) - 1;
-	tab->cmd = ft_strjoin(bf_var, aft_var);
-	if (!tab->cmd)
-		return (free(bf_var), free(aft_var), ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
-	free(bf_var);
-	free(aft_var);
-	return (1);
-}
+// 	size = ft_strlen(tab->cmd);
+// 	bf_var = ft_substr(tab->cmd, 0, *i - 1);
+// 	aft_var = ft_substr(tab->cmd, *i + j, size - (*i + j));
+// 	if (!bf_var || !aft_var)
+// 		return (free(bf_var), free(aft_var), ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
+// 	free(tab->cmd);*i = ft_strlen(bf_var) - 1;
+// 	tab->cmd = ft_strjoin(bf_var, aft_var);
+// 	if (!tab->cmd)
+// 		return (free(bf_var), free(aft_var), ft_free_cmd_tab(tab), ft_free(md), ft_exit('m'), 0);
+// 	free(bf_var);
+// 	free(aft_var);
+// 	return (1);
+// }
 
-int	ft_expand_var(char* line)
-{
-	char 	*bf_var;
-	char 	*aft_var;
-	char	*tmp;
-	int		size;
+// int	ft_expand_var(char* line)
+// {
+// 	char 	*bf_var;
+// 	char 	*aft_var;
+// 	char	*tmp;
+// 	int		size;
 
-	size = ft_strlen(line);
-	bf_var = ft_substr(line, 0, *i - 1);
-	aft_var = ft_substr(line, *i + j, size - (*i + j));
-	if (!bf_var || !aft_var)
-		return (free(bf_var), free(aft_var), ft_exit('m'), 0);
-	tmp = ft_strjoin(bf_var, variable->content);
-	if (!tmp)
-		return (free(bf_var), free(aft_var), ft_exit('m'), 0);
-	free(bf_var);
-	*i = ft_strlen(tmp) - 1;
-	free(line);
-	line = ft_strjoin(tmp, aft_var);
-	if (!line)
-		return (free(tmp), free(aft_var), ft_exit('m'), 0);
-	free(tmp);
-	free(aft_var);
-	return (1);
-}
+// 	size = ft_strlen(line);
+// 	bf_var = ft_substr(line, 0, *i - 1);
+// 	aft_var = ft_substr(line, *i + j, size - (*i + j));
+// 	if (!bf_var || !aft_var)
+// 		return (free(bf_var), free(aft_var), ft_exit('m'), 0);
+// 	tmp = ft_strjoin(bf_var, variable->content);
+// 	if (!tmp)
+// 		return (free(bf_var), free(aft_var), ft_exit('m'), 0);
+// 	free(bf_var);
+// 	*i = ft_strlen(tmp) - 1;
+// 	free(line);
+// 	line = ft_strjoin(tmp, aft_var);
+// 	if (!line)
+// 		return (free(tmp), free(aft_var), ft_exit('m'), 0);
+// 	free(tmp);
+// 	free(aft_var);
+// 	return (1);
+// }
